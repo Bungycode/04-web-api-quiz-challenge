9647e4d2919d681796e602b450c787c1e2508693
Andrew Bungcayao <thedevandrew@gmail.com>
2022-03-26 18:38:58 -0400

    Adding html and js query selectors

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Create global javaScript selectors
+var screen0Ele = document.querySelector('#screen0');
+var screen0BtnEle = screen0Ele.querySelector('button');
+var screen1Ele = document.querySelector('#screen1');
+var screen1BtnEle = screen1Ele.querySelector('button');
+var screen2Ele = document.querySelector('#screen2');
+var screen2ButtonEle = screen2Ele.querySelector('button');
+var question = document.querySelector('#question');
+var potentialAnswers = document.querySelector('#potentialAnswers');
+var doSomething = document.querySelector('#doSomething');
+var saySomething = document.querySelector('#saySomething');
